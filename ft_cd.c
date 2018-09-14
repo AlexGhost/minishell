a56f484f54910eef4a275267100dbe9ff1d539af
acourtin <acourtin@student.42.fr>
2018-09-14 17:00:40 +0200

    Norme

--- ft_cd.c
+++ ft_cd.c
@@ -6,11 +6,22 @@
 /*   By: acourtin <acourtin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/21 14:07:35 by acourtin          #+#    #+#             */
-/*   Updated: 2018/09/12 16:55:22 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/09/14 16:41:57 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void		change_env_pwd(char *cwd, t_lstenv *oldpwd, t_lstenv *pwd)
+{
+	getcwd(cwd, 4096);
+	ft_putendl(cwd);
+	ft_strdel(&oldpwd->value);
+	oldpwd->value = ft_strjoin(pwd->value, "");
+	ft_strdel(&pwd->value);
+	pwd->value = ft_strjoin(cwd, "");
+	ft_strdel(&cwd);
+}
 
 void			ft_cd(char **tab, t_lstenv **envv)
 {
@@ -20,7 +31,7 @@ void			ft_cd(char **tab, t_lstenv **envv)
 	t_lstenv	*pwd;
 	t_lstenv	*home;
 
- 	home = search_key(*envv, "HOME");
+	home = search_key(*envv, "HOME");
 	oldpwd = search_key(*envv, "OLDPWD");
 	pwd = search_key(*envv, "PWD");
 	if (tab[1])
@@ -36,14 +47,6 @@ void			ft_cd(char **tab, t_lstenv **envv)
 	{
 		cwd = ft_strnew(4096);
 		if (pwd && oldpwd)
-		{
-			getcwd(cwd, 4096);
-			ft_putendl(cwd);
-			ft_strdel(&oldpwd->value);
-			oldpwd->value = ft_strjoin(pwd->value, "");
-			ft_strdel(&pwd->value);
-			pwd->value = ft_strjoin(cwd, "");
-			ft_strdel(&cwd);
-		}
+			change_env_pwd(cwd, oldpwd, pwd);
 	}
 }
